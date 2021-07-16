0a7ad9dd03631b530a43f785e3c6d8d6f4f44d69
hoobab <87278869+good-boi-huan@users.noreply.github.com>
2021-07-16 21:15:37 +0000

    warm up

---
+++ main.py
@@ -0,0 +1,13 @@
+# import turtle
+# z = turtle.Turtle()
+
+# def polygon(sides, length):
+#   angle = 360.0/sides
+#   for i in range(0, sides):
+#     z.forward(length)
+#     z.right(angle)
+
+# sides = int(input("How many sides do you want your polygon to have? "))
+# length = int(input("How long are the sides? "))
+
+# polygon(sides, length)
